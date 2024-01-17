e39a8306572fab8b94ec67129c1d3cd6dc272311
an4lex <an4lex@gmail.com>
2024-01-17 16:53:29 +0200

    Initial Commit

---
+++ GleamCar/GleamCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GleamCarApp.swift
+//  GleamCar
+//
+//  Created by Alexander Andrianov on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GleamCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
