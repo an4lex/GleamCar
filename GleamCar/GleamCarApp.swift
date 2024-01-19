f13d3689d38e29d40513e4b73a5d15b1241ad4ca
an4lex <an4lex@gmail.com>
2024-01-19 20:03:54 +0200

    Minor changes.

--- GleamCar/GleamCarApp.swift
+++ GleamCar/GleamCarApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct GleamCarApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      MainTabView()
     }
+  }
 }
